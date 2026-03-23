aeb45ae24c37b52ad857ee4c07893f7673e3783d
Xeffen25 <xeffen25@xeffen25.com>
2026-03-23 23:54:10 +0100

    feat: add ColorScheme and ThemeColor components

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
+import Base from "@/components/Base.astro";
+import ColorScheme from "@/components/ColorScheme.astro";
 import Description from "@/components/Description.astro";
 import Head from "@/components/Head.astro";
 import Robots from "@/components/Robots.astro";
+import ThemeColor from "@/components/ThemeColor.astro";
 import Title from "@/components/Title.astro";
 
-export { Description, Head, Robots, Title };
+export { Base, ColorScheme, Description, Head, Robots, ThemeColor, Title };
