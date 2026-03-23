ce7e79c2ab81aa70f68fc644e1a17332b989fe3c
Xeffen25 <xeffen25@xeffen25.com>
2026-03-23 22:31:00 +0100

    feat: add initial components Head, Description, and Title

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
+import Description from "@/components/Description.astro";
+import Head from "@/components/Head.astro";
+import Title from "@/components/Title.astro";
 
-import MyComponent from "./src/MyComponent.astro";
-
-export default MyComponent;
+export { Head, Description, Title };
