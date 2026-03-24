56cf8062ae75d9e69ace26543acd177109cfa367
Xeffen25 <xeffen25@xeffen25.com>
2026-03-24 12:19:12 +0100

    feat: add alternate component

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
+import Alternate from "@/components/Alternate.astro";
 import AppleWebApp from "@/components/AppleWebApp.astro";
 import Base from "@/components/Base.astro";
 import Canonical from "@/components/Canonical.astro";
@@ -13,6 +14,7 @@ import Title from "@/components/Title.astro";
 import Verification from "@/components/Verification.astro";
 
 export {
+	Alternate,
 	AppleWebApp,
 	Base,
 	Canonical,
